c36338a62cd02b358e4cc36e531701639b54e4ea
avrilnunez <dulceavrilnunezmier@gmail.com>
2026-03-26 08:44:41 -0600

    Translate Fecha3.cpp to Spanish and update syntax

--- Fecha3.cpp
+++ Fecha3.cpp
@@ -1,36 +1,40 @@
-#include <windows.h>
+#include <ventanas.h>
 #include <iostream>
-using namespace std;
-int main()
+usando espacio de nombres std;
+int principal()
 
 {
 	SetConsoleOutputCP(CP_UTF8);
-	SetConsoleCP(CP_UTF8);
+	Establecer consola CP(CP_UTF8);
 	int d,m,a;
-	string mes;
-	cout<<"Dame una fecha en este formato: ddmmaaaa";
-	scanf("%20%26%4d", &d, &m,&a);
-	if (m==1)
-		mes="Enero";
-	else if (m==4)
-		mes="Abril";
-	else if (m==5)
-		mes="Mayo";
-	else if (m==6)
-		mes="Junio";
-	else if (m==7)
-		mes="Julio";
-	else if (m==8)
-		mes="Agosto";
-	else if (m==9)
-		mes="Septiembre";
-	else if (m==10)
-		mes="Octubre";
-	else if (m==11)
-		mes="Noviembre";
-	else if (m==12)
-		mes="Diciembre";
-	else mes="Mes Inválido";
-	cout<<"La fecha es: "<<d<<"-"<<mes<<"-"<<a;
-	return 0;
+	cadena mes;
+cout<<"Dame una fecha en este formato: ddmmaaaa";
+	scanf("%20%26%4d", yd, ym, ya);
+	si (m==1)
+mes="Enero";
+	else si (m==2)
+mes=„Febrero";
+de lo contrario si (m==3)
+mes="Marzo";
+de lo contrario si (m==4)
+mes="Abril";
+de lo contrario si (m==5)
+mes="Mayo";
+de lo contrario si (m==6)
+mes=„Junio";
+de lo contrario si (m==7)
+mes=„Julio";
+de lo contrario si (m==8)
+mes=„Agosto";
+de lo contrario si (m==9)
+mes="Septiembre";
+de lo contrario si (m==10)
+mes="Octubre";
+de lo contrario si (m==11)
+mes="Noviembre";
+de lo contrario si (m==12)
+mes="Diciembre";
+de lo contrario mes="Mes InvÁlido";
+cout<<"La fecha es: "<<d<<"-"<<mes<<"-"<<a;
+retorno 0;
 }
