42baa5258132b43f6e954db0353800480b625e06
avrilnunez <dulceavrilnunezmier@gmail.com>
2026-03-25 20:08:41 -0600

    Add files via upload

---
+++ ApruebaABCD.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+int main()
+{
+	SetConsoleOutputCP(CP_UTF8);
+	SetConsoleCP(CP_UTF8);
+	string nombre;
+	int cal1,cal2,cal3,prom;
+	cout<<"Dame tu nombre: ";
+	cin>>nombre;
+	cout<<"Dame la calificación 1: ";
+	cin>>cal1;
+	cout<<"Dame la calificación 2: ";
+	cin>>cal2;
+	cout<<"Dame la calificación 3: ";
+	cin>>cal3;
+	prom=(cal1+cal2+cal3)/3;
+	cout<<nombre<<", tu promedio es; "<<prom<<"y tu calificación final es: ";
+	if(prom>=90 && prom<=100)
+	cout<<"A";
+	else
+	if(prom>=80 && prom<=89)
+	cout<<"B";
+	else
+	if(prom>=70 && prom<=79)
+	cout<<"C";
+	else
+	if(prom>=60 && prom<=69)
+	cout<<"D";
+	else
+	if(prom>=0 && prom<=59)
+	cout<<"N/A";
+	else
+	cout<<"Valor invalidos";
+	return 0;
+}
+
