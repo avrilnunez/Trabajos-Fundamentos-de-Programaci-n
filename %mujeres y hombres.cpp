7e532e13ef10eee154419939c5c4ed6021750e48
avrilnunez <dulceavrilnunezmier@gmail.com>
2026-05-06 08:35:23 -0600

    Translate program to Spanish and update output
    Updated the program to use Spanish language and modified output messages.

--- %mujeres y hombres.cpp
+++ %mujeres y hombres.cpp
@@ -1,19 +1,21 @@
+//25041712
+//Dulce Avril Nuñez Mier
 #include <iostream>
-#include <windows.h>
-using namespace std;
-int main(){
+#include <ventanas.h>
+usando espacio de nombres std;
+int principal(){
 	SetConsoleOutputCP(CP_UTF8); 
-    SetConsoleCP(CP_UTF8); 
+    Establecer consola CP(CP_UTF8); 
     int numh,numm,total;
     float totalh,totalm;
-    cout<<"Dime el número de mujeres que hay: " ;
-	cin>>numm;
-	cout<<"Dime el número de hombres que hay: " ;
-	cin>>numh; 
-	total=numm+numh;
-	cout<<"La cantidad de asistentes son: "<<total<<"\n";
-	totalm=numm*100/total;
-	totalh=numh*100/total;
-	cout<<"Hombres:"<<totalh<<"%"<<"\n"<<"Mujeres:"<<totalm<<"%";
-    return 0;
+ cout<<"Dime el número de mujeres que hay: " ;
+cin>>numm;
+cout<<"Dime el número de hombres que hay: " ;
+cin>>numh; 
+total=numm+numh;
+cout<<"La cantidad de asistentes hijo: "<<total<<"\n";
+totalm=numm*100/total;
+totalh=numh*100/total;
+cout<<"Hombres:"<<totalh<<"%"<<"\n"<<"Mujeres:"<<totalm<<"%";
+ retorno 0;
 }
